713cc6b0581d4c7828b731253c960036f77d616d
Gunnar Unger <gunnar.unger@gmail.com>
2022-10-07 14:55:40 +1100

    update

--- module-list.js
+++ module-list.js
@@ -5,6 +5,8 @@
         "not_elig3":         {url:"$H/m/not_elig.html"},
         "recruitment-data": {url:"$H/m/elig-questions-data.html",Table:"cbn-insomnia-recruitment-record",form_module:"recruitment-form",router:1},
         "recruitment-form": {url:"$H/m/elig-questions-form.html",Table:"cbn-insomnia-recruitment-record"},
+        "cupid-from-canrest-data":{url:"$H/m/cupid-from-canrest-data.html",Table:"cupid-from-canrest",form_module:"cupid-from-canrest-form",task_name:"From CANRest"},
+        "cupid-from-canrest-form":{url:"$H/m/cupid-from-canrest-form.html",Table:"cupid-from-canrest",task_name:"From CANRest"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p];
